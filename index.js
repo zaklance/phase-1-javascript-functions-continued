fd1565b8f760c039caa5eacb976160e0df10bf57
zaklance <wosewick@gmail.com>
2024-04-21 09:02:04 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+};
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+};
+function wrapAdjective(adj = 'special'){
+    return function(symb = '*'){
+        return `You are ${adj}${symb}${adj}!`;
+    };
+};
+
+
+
+
+
+// function wrapAdjective(adj = 'special'){
+//     return function(symb = '*'){
+//         return `You are ${symb}${adj}${symb}!`;
+//     };
+// };
+
+
+
+// function wrapAdjective(adj = 'special'){
+//     return function(){
+//         let symb = '*';
+//     return `You are ${symb}${adj}${symb}!`;
+//     };
+// };
